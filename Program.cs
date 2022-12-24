8548ab706f4180c7bd0282f7ef1d63232377d63a
Mohamed-abdelrahman22 <121330401+Mohamed-abdelrahman22@users.noreply.github.com>
2022-12-24 07:39:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ThePrimeNumbers ;
+public class program
+{
+    public static void Main(string[]args)
+    {
+        int x, y, z, first, second;
+        Console.Write("Enter Starting Number of range: ");
+        first = int.Parse(Console.ReadLine());
+        Console.Write("Enter Ending Number of range : ");
+        second = int.Parse(Console.ReadLine());
+        Console.WriteLine("The prime numbers between {0} and {1} are :", first, second);
+        for (x = first; x <= second; x++)
+        {
+         z = 0;
+         for (y = 2 ; y <= x / 2; y++)
+         {
+          if (x % y == 0)
+           {
+            z++;
+            break;
+          }
+         }
+          if (z == 0 && x != 1)
+          Console.Write("{0} ", x);
+        }
+      
+    }
+}
+
